c1cf75ce1c6e7541a29fc200da382e446a83655c
Hase Florian <florian.hase@sap.com>
2022-09-28 20:04:40 +0200

    added routes and devView|started chat

---
+++ lib/views/chat.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ChatView extends StatefulWidget {
+  const ChatView({super.key});
+
+  @override
+  State<ChatView> createState() => _ChatViewState();
+}
+
+class _ChatViewState extends State<ChatView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Chat')), body: Container());
+  }
+}
