a76c3140cc73a5f971c1aa0a2bd086e00c4077a0
Hase Florian <florian.hase@sap.com>
2022-09-29 13:16:02 +0200

    added implicit constructor for Message

--- lib/helpers/message.dart
+++ lib/helpers/message.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class Message {
   final String sender;
   final String receiver;
@@ -5,4 +7,10 @@ class Message {
   final String content;
 
   Message._(this.sender, this.receiver, this.timeStamp, this.content);
+
+  static Message fromJson(String jsonString) {
+    Map<String, String> message = json.decode(jsonString);
+    return Message._(message['sender']!, message['receiver']!,
+        message['timeStamp']!, message['content']!);
+  }
 }
