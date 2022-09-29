084cbea1a9e14f1b3e293c20ab5ca96737fe703a
Hase Florian <florian.hase@sap.com>
2022-09-29 13:06:09 +0200

    added message class w/ private constr

---
+++ lib/helpers/message.dart
@@ -0,0 +1,8 @@
+class Message {
+  final String sender;
+  final String receiver;
+  final String timeStamp;
+  final String content;
+
+  Message._(this.sender, this.receiver, this.timeStamp, this.content);
+}
