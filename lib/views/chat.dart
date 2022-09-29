04a9d899b2758a8727b5bfda8cd0c2422338e331
Hase Florian <florian.hase@sap.com>
2022-09-29 12:08:01 +0200

    added Send Button

--- lib/views/chat.dart
+++ lib/views/chat.dart
@@ -8,9 +8,41 @@ class ChatView extends StatefulWidget {
 }
 
 class _ChatViewState extends State<ChatView> {
+  var messages = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Chat')), body: Container());
+        appBar: AppBar(title: const Text('Chat')),
+        body: Column(
+          children: [
+            Expanded(
+              flex: 25,
+              child: Container(
+                color: Colors.black,
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(3),
+                child: Row(
+                  children: [
+                    const Expanded(
+                      flex: 10,
+                      child: TextField(
+                        decoration:
+                            InputDecoration(border: OutlineInputBorder()),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.send),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
