3ab9f437df424d07327c444ca7a3ab7e51a5011c
Hase Florian <florian.hase@sap.com>
2022-09-29 20:15:07 +0200

    added timestamp on message widget

--- lib/widgets/chat_message.dart
+++ lib/widgets/chat_message.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/helpers/datetime_formatter.dart';
+import 'package:frontend/helpers/message.dart';
 
-Widget chatMessage(String content, EdgeInsetsGeometry edgeInsetsGeometry) {
+Widget chatMessage(MyMessage message, EdgeInsetsGeometry edgeInsetsGeometry) {
+  var date = "";
+  try {
+    date = DateTimeFormatter.DDMMYYYY_hhmmss(DateTime.parse(message.timestamp));
+  } catch (e) {
+    e.toString();
+  }
+
   return Padding(
     padding: edgeInsetsGeometry,
     child: Container(
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(5)),
-        color: Colors.green,
-        boxShadow: [
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(5)),
+        color: Colors.green.withOpacity(0.7),
+        boxShadow: const [
           BoxShadow(
             color: Colors.grey,
             offset: Offset(3, 3),
@@ -18,10 +27,23 @@ Widget chatMessage(String content, EdgeInsetsGeometry edgeInsetsGeometry) {
       ),
       width: 15,
       child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: Text(
-          content,
-          style: const TextStyle(fontSize: 25),
+        padding: const EdgeInsets.all(5),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                message.content,
+                style: const TextStyle(fontSize: 25),
+              ),
+            ),
+            Align(
+                alignment: Alignment.bottomRight,
+                child: Text(
+                  date,
+                  style: TextStyle(color: Colors.grey.shade800, fontSize: 15),
+                ))
+          ],
         ),
       ),
     ),
