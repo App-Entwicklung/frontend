53734847a5e15fea9d412b076c531a052a90ad41
Hase Florian <florian.hase@sap.com>
2022-09-29 13:57:13 +0200

    added rough messaging setup

--- lib/views/chat.dart
+++ lib/views/chat.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/helpers/message.dart';
 
 class ChatView extends StatefulWidget {
   const ChatView({super.key});
@@ -8,7 +9,18 @@ class ChatView extends StatefulWidget {
 }
 
 class _ChatViewState extends State<ChatView> {
-  var messages = [];
+  var messages = []; //TODO: Fill onInit
+  var myUserID = ""; //TODO: As input param
+  var contactUserID = ""; //TODO: As input param
+  var messageContent = "";
+
+  sendMessage() {
+    if (messageContent == "") {
+      return;
+    }
+    MyMessage.fromValues(myUserID, contactUserID,
+        DateTime.now().toIso8601String(), messageContent);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,15 +39,24 @@ class _ChatViewState extends State<ChatView> {
                 padding: const EdgeInsets.all(3),
                 child: Row(
                   children: [
-                    const Expanded(
+                    Expanded(
                       flex: 10,
                       child: TextField(
+                        onChanged: (value) {
+                          messageContent = value;
+                        },
+                        onSubmitted: (value) {
+                          messageContent = value;
+                          sendMessage();
+                        },
                         decoration:
-                            InputDecoration(border: OutlineInputBorder()),
+                            const InputDecoration(border: OutlineInputBorder()),
                       ),
                     ),
                     IconButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        sendMessage();
+                      },
                       icon: const Icon(Icons.send),
                     )
                   ],
