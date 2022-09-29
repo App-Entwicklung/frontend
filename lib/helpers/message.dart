debe537aff679029b067bf9d03b7c89b0403562b
Hase Florian <florian.hase@sap.com>
2022-09-29 13:40:54 +0200

    added toJsonString method

--- lib/helpers/message.dart
+++ lib/helpers/message.dart
@@ -3,14 +3,25 @@ import 'dart:convert';
 class Message {
   final String sender;
   final String receiver;
-  final String timeStamp;
+  final String timestamp;
   final String content;
 
-  Message._(this.sender, this.receiver, this.timeStamp, this.content);
+  Message._(this.sender, this.receiver, this.timestamp, this.content);
 
   static Message fromJson(String jsonString) {
     Map<String, String> message = json.decode(jsonString);
     return Message._(message['sender']!, message['receiver']!,
         message['timeStamp']!, message['content']!);
+  }
+
+  String toJsonString() {
+    return '''
+    {
+      "sender": $sender,
+      "receiver": $receiver,
+      "timestamp": $timestamp,
+      "content": $content
+    }
+    ''';
   }
 }
