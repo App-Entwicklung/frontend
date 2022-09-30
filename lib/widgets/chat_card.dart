7d4b003039f70a73100c1e75dcc574a4d02ffa36
Rebekka Miguez <rebekka.miguez@gmx.de>
2022-09-30 16:40:58 +0200

    Chat Card Text

--- lib/widgets/chat_card.dart
+++ lib/widgets/chat_card.dart
@@ -10,7 +10,25 @@ Widget chatCard(Chat chat) => Padding(
             radius: 24,
             backgroundImage: NetworkImage(chat.image),
             backgroundColor: Colors.transparent,
-          )
+          ),
+          Expanded(
+              child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(chat.name,
+                          style: const TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          )),
+                      Text(
+                        chat.lastMessage,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      )
+                    ],
+                  )))
         ],
       ),
     );
