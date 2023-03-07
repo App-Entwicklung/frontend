12a2fa9fa6d5bc54a9c7e36fbed7afe75cdd264e
HaseFlrn <florianhase12@gmail.com>
2023-03-07 13:57:22 +0100

    fixed render error

--- lib/views/testview.dart
+++ lib/views/testview.dart
@@ -15,138 +15,158 @@ class _TestState extends State<Test> {
   late String _value4;
   late String _value5;
   late String _value6;
-  
+
+  @override
+  void initState() {
+    _value1 = "";
+    _value2 = "";
+    _value3 = "";
+    _value4 = "";
+    _value5 = "";
+    _value6 = "";
+
+    super.initState();
+  }
+
+  Widget nest(Widget child1, Widget child2, String text) {
+    return Row(
+      children: [
+        Expanded(
+            flex: 5,
+            child:
+                Padding(padding: const EdgeInsets.all(8.0), child: Text(text))),
+        Expanded(
+            flex: 10,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: child1,
+            )),
+        Expanded(
+            child: Padding(padding: const EdgeInsets.all(8.0), child: child2))
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView(children: [
-      Row(
-        children: [
-          const Text("create Account"),
-          TextField(
-            onChanged: ((value) => {
-                  setState(
-                    () => {_value1 = value},
-                  )
-                }),
-          ),
-          ElevatedButton(
-              onPressed: () async => {await Contract.createAccount(_value1)},
-              child: const Text("Go"))
-        ],
-      ),
-      Row(
-        children: [
-          const Text("sendContactRequest"),
-          TextField(
-            onChanged: ((value) => {
-                  setState(
-                    () => {_value2 = value},
-                  )
-                }),
-          ),
-          ElevatedButton(
-              onPressed: () async =>
-                  {await Contract.sendContactRequest(_value2)},
-              child: const Text("Go"))
-        ],
-      ),
-      Row(
-        children: [
-          const Text("acceptContactRequest"),
-          TextField(
-            onChanged: ((value) => {
-                  setState(
-                    () => {_value3 = value},
-                  )
-                }),
-          ),
-          ElevatedButton(
-              onPressed: () async =>
-                  {await Contract.acceptContactRequest(_value3)},
-              child: const Text("Go"))
-        ],
-      ),
-      Row(
-        children: [
-          const Text("getReceivedContactRequests"),
-          TextField(
-            onChanged: ((value) => {
-                  setState(
-                    () => {_value4 = value},
-                  )
-                }),
-          ),
-          ElevatedButton(
-              onPressed: () async {
-                final result = await Contract.getReceivedContactRequests();
-                showDialog(
-                    context: context,
-                    builder: ((context) {
-                      return Container(
-                        color: Colors.red,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            result.toString(),
-                            style: const TextStyle(
-                                fontSize: 15, fontWeight: FontWeight.bold),
+    return Scaffold(
+        appBar: AppBar(title: const Text("Test Contract Methods")),
+        body: Column(children: [
+          nest(
+              TextField(
+                onChanged: ((value) => {
+                      setState(
+                        () => {_value1 = value},
+                      )
+                    }),
+              ),
+              ElevatedButton(
+                onPressed: () async => {await Contract.createAccount(_value1)},
+                child: const Text("Go"),
+              ),
+              "create Account"),
+          nest(
+              TextField(
+                onChanged: ((value) => {
+                      setState(
+                        () => {_value2 = value},
+                      )
+                    }),
+              ),
+              ElevatedButton(
+                onPressed: () async =>
+                    {await Contract.sendContactRequest(_value2)},
+                child: const Text("Go"),
+              ),
+              "sendContactRequest"),
+          nest(
+              TextField(
+                onChanged: ((value) => {
+                      setState(
+                        () => {_value3 = value},
+                      )
+                    }),
+              ),
+              ElevatedButton(
+                onPressed: () async =>
+                    {await Contract.acceptContactRequest(_value3)},
+                child: const Text("Go"),
+              ),
+              "acceptContactRequest"),
+          nest(
+              TextField(
+                onChanged: ((value) => {
+                      setState(
+                        () => {_value4 = value},
+                      )
+                    }),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final result = await Contract.getReceivedContactRequests();
+                  showDialog(
+                      context: context,
+                      builder: ((context) {
+                        return Container(
+                          color: Colors.red,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              result.toString(),
+                              style: const TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.bold),
+                            ),
                           ),
-                        ),
-                      );
-                    }));
-              },
-              child: const Text("Go"))
-        ],
-      ),
-      Row(
-        children: [
-          const Text("sendMessage"),
-          TextField(
-            onChanged: ((value) => {
-                  setState(
-                    () => {_value5 = value},
-                  )
-                }),
-          ),
-          ElevatedButton(
-              onPressed: () async =>
-                  {await Contract.sendMessage(_value5, "HI")},
-              child: const Text("Go"))
-        ],
-      ),
-      Row(
-        children: [
-          const Text("getMessages"),
-          TextField(
-            onChanged: ((value) => {
-                  setState(
-                    () => {_value6 = value},
-                  )
-                }),
-          ),
-          ElevatedButton(
-              onPressed: () async {
-                final result = await Contract.getMessages(_value6);
-                showDialog(
-                    context: context,
-                    builder: ((context) {
-                      return Container(
-                        color: Colors.red,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            result.toString(),
-                            style: const TextStyle(
-                                fontSize: 15, fontWeight: FontWeight.bold),
+                        );
+                      }));
+                },
+                child: const Text("Go"),
+              ),
+              "getReceivedContactRequests"),
+          nest(
+              TextField(
+                onChanged: ((value) => {
+                      setState(
+                        () => {_value5 = value},
+                      )
+                    }),
+              ),
+              ElevatedButton(
+                onPressed: () async =>
+                    {await Contract.sendMessage(_value5, "HI")},
+                child: const Text("Go"),
+              ),
+              "sendMessage"),
+          nest(
+              TextField(
+                onChanged: ((value) => {
+                      setState(
+                        () => {_value6 = value},
+                      )
+                    }),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final result = await Contract.getMessages(_value6);
+                  showDialog(
+                      context: context,
+                      builder: ((context) {
+                        return Container(
+                          color: Colors.red,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              result.toString(),
+                              style: const TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.bold),
+                            ),
                           ),
-                        ),
-                      );
-                    }));
-              },
-              child: const Text("Go"))
-        ],
-      ),
-    ]);
+                        );
+                      }));
+                },
+                child: const Text("Go"),
+              ),
+              "getMessages"),
+        ]));
   }
 }
